a2b7fc30bf995b2e2677c67ec24324d7dd32d2d1
= <=>
2023-02-03 18:46:25 -0300

    add tests to withdraw function

--- src/test/entities/AccountTests.java
+++ src/test/entities/AccountTests.java
@@ -38,6 +38,25 @@ public class AccountTests {
 	}
 	
 	@Test
+	public void withdrawShouldDecreaseBalanceWhenSufficientBalance() { 
+		Account acc = AccountFactory.createAccount(800.0);
+		
+		acc.withdraw(500.0);
+		
+		Assertions.assertEquals(300.0, acc.getBalance());
+	}
+	
+	@Test
+	public void withdrawShouldThrowExceptionWhenInsufficientBalance() { 
+		
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			Account acc = AccountFactory.createAccount(800.0);
+			acc.withdraw(801.0);
+		});
+	}
+	
+	
+	@Test
 	public void fullWithdrawShouldClearBalanceAndReturnFullBalance() { 
 		
 		double expectedValue = 0.0;
