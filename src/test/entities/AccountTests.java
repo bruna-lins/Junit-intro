c7c18a35b67a38ec4d8c18c0b7c4140f9e71156c
= <=>
2023-02-03 18:21:22 -0300

    add test depositShouldDoNothingWhenNegativeAmount

--- src/test/entities/AccountTests.java
+++ src/test/entities/AccountTests.java
@@ -8,11 +8,26 @@ import entities.Account;
 public class AccountTests {
 
 	@Test
-	public void depositShouldIncreaseBalanceWhenPositiveAmount() { 
+	public void depositShouldIncreaseBalanceAndDiscountFeeWhenPositiveAmount() { 
 		//ARRANGE
 		double amount = 200.0;
 		double expectedValue = 196.0;
 		Account acc = new Account(1L, 0.0);
+		
+		//ACT
+		acc.deposit(amount);
+		
+		//ASSERT
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
+	
+	@Test
+	public void depositShouldDoNothingWhenNegativeAmount() { 
+		
+		//ARRANGE
+		double expectedValue = 100.0;
+		Account acc = new Account(1L, expectedValue);
+		double amount = -200.0;
 		
 		//ACT
 		acc.deposit(amount);
